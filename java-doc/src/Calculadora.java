dd1ee186e789610a1f826d4f4ad1a720c3001060
grittiDM <dev.murilogritti@gmail.com>
2024-06-18 18:06:41 -0300

    Estudos de comentários e utilização de Javadoc

---
+++ java-doc/src/Calculadora.java
@@ -0,0 +1,34 @@
+/**
+* <h1>Calculadora</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author Gleyson Sampaio
+* @version 1.0
+* @since 01/01/2022
+*/
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param a Primeiro número a ser somado
+     * @param b Segundo número a ser somado
+     * @return int o resultado do cálculo de a + b
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+    /**
+     * Este método é utilizado para subtrair dois números inteiros
+     * @param a Primeiro número a ser subtraído
+     * @param b Segundo número a ser subtraído
+     * @return int o resultado do cálculo de a - b
+     */
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+    /**
+     * Este método é utilizado para multiplicar dois números inteiros
+     */
+}
---
+++ java-doc/src/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, este é um comentário de única linha
+
+        /*
+         * Este é um comentário
+         * de múltiplas linhas
+         */
+
+        /**
+         * Este é um comentário
+         * de documentação
+         * para propriedades, métodos e classes
+         */
+    }
+}
