a6c50d63b79998669d43f0525ef0799322c876a1
grittiDM <dev.murilogritti@gmail.com>
2024-06-18 19:12:38 -0300

    Finalizado o estudo de sintaxe do java

--- terminal-e-argumentos/src/AboutMe.java
+++ terminal-e-argumentos/src/AboutMe.java
@@ -1,14 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
         
-        //os argumetnos comçam com indice 0
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]); //converte para inteiro
-        double altura = Double.valueOf(args[3]);
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
 
-        System.out.println("Prazer, meu nome é: " + nome + " " + sobrenome);
-        System.out.println("Minha idade é: " + idade + " anos");
-        System.out.println("Minha altura é: " + altura +  "cm");
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        //imprimindo os dados obtidos
+        System.out.println("Prazer " + nome + " " + sobrenome + " espero que esteja bem!");
+        System.out.println("Seu sobrenome " + sobrenome + " é muito interessante");
+        System.out.println("Você tem " + idade + " anos");
+        System.out.println("E sua altura é " + altura + "m");
     }
 }
